b956c059126b99f7993a715c5cf033204d12dbc8
linhvt2503 <vtlinh02052003@gmail.com>
2024-03-24 22:30:23 +0700

    gk

--- Cau2_AppCalculator/app/src/main/java/ntu/linh63132961/cau2_appcalculator/MainActivity.java
+++ Cau2_AppCalculator/app/src/main/java/ntu/linh63132961/cau2_appcalculator/MainActivity.java
@@ -14,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     EditText editText1;
     EditText editText2;
-    Button nutCong, nutTru, nutNhan, nutChia, nutMu2, nutPi, nutE,
+    Button nutCong, nutTru, nutNhan, nutChia, nutMu2, nutPi, nutDel,
             nutAm, nutPhantram, nutPhay, nutBang, nutAc,
             nut1, nut2, nut3, nut4, nut5, nut6, nut7, nut8, nut9, nut0;
     double v1, v2, ans;
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         nutChia = (Button) findViewById(R.id.btnchia);
         nutPi = (Button) findViewById(R.id.btnpi);
         nutMu2 = (Button) findViewById(R.id.btnmu2);
-        nutE = (Button) findViewById(R.id.btne);
+        nutDel = (Button) findViewById(R.id.btnDel);
         nut0 = (Button) findViewById(R.id.btn0);
         nut1 = (Button) findViewById(R.id.btn1);
         nut2 = (Button) findViewById(R.id.btn2);
@@ -50,12 +50,80 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timDieuKien();
-        View.OnClickListener BoLangNghe = new View.OnClickListener() {
+
+
+        nut0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //x ly cong
+                editText1.setText(editText1.getText()+"0");
+
+            }
+        });
+        nut1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 editText1.setText(editText1.getText()+"1");
             }
-        }
+        });
+        nut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"2");
+            }
+        });
+        nut3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"3");
+            }
+        });
+        nut4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"4");
+            }
+        });
+        nut5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"5");
+            }
+        });
+        nut6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"6");
+            }
+        });
+        nut7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"7");
+            }
+        });
+        nut8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"8");
+            }
+        });
+        nut9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+"9");
+            }
+        });
+        nutPhay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(editText1.getText()+".");
+            }
+        });
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
