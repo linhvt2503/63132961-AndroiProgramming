73258f1bb1d7f9794154f7b212c32433f4084cc7
linhvt2503 <vtlinh02052003@gmail.com>
2024-03-25 17:43:45 +0700

    .

--- Cau3_MyProfile/app/src/main/java/ntu/linh63132961/cau3_myprofile/MainActivity.java
+++ Cau3_MyProfile/app/src/main/java/ntu/linh63132961/cau3_myprofile/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,17 +13,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button xemChiTiet = (Button) findViewById(R.id.btn);
+    TextView textView3 =(TextView) findViewById(R.id.txt3);
+    TextView textView4 =(TextView) findViewById(R.id.txt4);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button xemChiTiet = findViewById(R.id.btn);
-        View.OnClickListener bolangnghe = new View.OnClickListener() {
+
+        xemChiTiet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                textView3.setText(textView3.getText()+"Lớp: 63CNTT.3");
+                textView4.setText(textView4.getText()+"Trường: NTU");
             }
-        };
+        });
     }
 }
