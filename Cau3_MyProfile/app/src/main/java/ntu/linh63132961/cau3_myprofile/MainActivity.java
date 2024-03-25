3c122e790b8bf89fd11bc39599c35e9c28fd47b2
linhvt2503 <vtlinh02052003@gmail.com>
2024-03-25 19:33:27 +0700

    gk

--- Cau3_MyProfile/app/src/main/java/ntu/linh63132961/cau3_myprofile/MainActivity.java
+++ Cau3_MyProfile/app/src/main/java/ntu/linh63132961/cau3_myprofile/MainActivity.java
@@ -13,19 +13,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button xemChiTiet = (Button) findViewById(R.id.btn);
-    TextView textView3 =(TextView) findViewById(R.id.txt3);
-    TextView textView4 =(TextView) findViewById(R.id.txt4);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button xemChiTiet = (Button) findViewById(R.id.btn);
+        TextView textView3 =(TextView) findViewById(R.id.txt3);
+        TextView textView4 =(TextView) findViewById(R.id.txt4);
         xemChiTiet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView3.setText(textView3.getText()+"Lớp: 63CNTT.3");
-                textView4.setText(textView4.getText()+"Trường: NTU");
+                String t="Lớp: 63CNTT.3";
+                String t1="Trường : NTU";
+                textView3.setText(t);
+                textView4.setText(t1);
+                xemChiTiet.setText("OK");
             }
         });
     }
