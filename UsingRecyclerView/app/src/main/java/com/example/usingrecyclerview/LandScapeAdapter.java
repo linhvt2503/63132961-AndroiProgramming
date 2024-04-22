888b65a5975b99d7d6470483c9d7846ddccf3a82
linhvt2503 <vtlinh02052003@gmail.com>
2024-04-22 14:55:43 +0700

    .

--- UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/LandScapeAdapter.java
+++ UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/LandScapeAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -51,7 +52,7 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
         return lstData.size();
     }
 
-    class ItemLandHolder extends RecyclerView.ViewHolder{
+    class ItemLandHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView tvCaption;
         ImageView ivLandScape;
 
@@ -59,7 +60,19 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
             super(itemView);
             tvCaption = itemView.findViewById(R.id.textViewCaption);
             ivLandScape = itemView.findViewById(R.id.imageViewLand);
+            itemView.setOnClickListener(this);
+        }
 
+        @Override
+        public void onClick(View v) {
+            int vitriduocclick=getAdapterPosition();
+            landScape phantuduocclick=lstData.get(vitriduocclick);
+            //bóc thông tin
+            String ten=phantuduocclick.getLandCaption();
+            String tenfile=phantuduocclick.getLandImageFileName();
+            //toast tên
+            String chuoithongbao="Bạn vừa click vào "+ten;
+            Toast.makeText(v.getContext(),chuoithongbao,Toast.LENGTH_SHORT).show();
         }
     }
 }
--- UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/MainActivity.java
+++ UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -20,6 +21,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //3
+        recyclerViewData = getRecylerViewDatas();
+        //4
+        recyclerViewLandScape=findViewById(R.id.recyclerLand);
+        //5
+//        RecyclerView.LayoutManager layoutLinear=new LinearLayoutManager(this);
+//        recyclerViewLand.setLayoutManager(layoutLinear);
+//        RecyclerView.LayoutManager layoutLinearHorizon=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+//        recyclerViewLand.setLayoutManager(layoutLinearHorizon);
+        RecyclerView.LayoutManager layoutLinearGrid=new GridLayoutManager(this,2);
+        recyclerViewLandScape.setLayoutManager(layoutLinearGrid);
+        //6
+        landScapeAdapter=new LandScapeAdapter(this,recyclerViewData);
+        //7
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<landScape>getRecylerViewDatas(){
+        ArrayList<landScape>dsdulieu=new ArrayList<landScape>();
+        landScape landScape=new landScape("meo2","Meooo meoo meoo");
+        dsdulieu.add(landScape);
+        dsdulieu.add(new landScape("meo1","Con mèo ngu ngốc"));
+        dsdulieu.add(new landScape("meo3","Đáng yêu, ngọt ngào"));
+        dsdulieu.add(new landScape("meo4","Cute phô mai que xin chào cả nhà"));
+        return dsdulieu;
     }
 }
